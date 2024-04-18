01b8e170c508247a3bb0db6056294f97bc999fc0
parikhitritgithub <parikhitkurmi44@gmail.com>
2024-04-18 18:37:29 +0530

    add all

---
+++ client/login_app/src/App.jsx
@@ -0,0 +1,54 @@
+import React from 'react'
+import './App.css'
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import Username from './components/Username';
+import Register from './components/Register';
+import Password from './components/Password';
+import Profile from './components/Profile';
+import Recovery from './components/Recovery';
+import Reset from './components/Reset';
+import PageNotFound from './components/PageNotFound';
+
+
+// for the  route purpose 
+
+const router = createBrowserRouter([
+
+  {
+      path : '/' , 
+      element : <Username></Username>
+  } ,
+  {
+      path : '/resgister' , 
+      element : <Register></Register>
+  } ,
+  {
+      path : '/password' , 
+      element : <Password></Password>
+  } ,
+  {
+      path : '/profile' , 
+      element : <Profile></Profile>
+  } ,
+  {
+      path : '/recovery' , 
+      element : <Recovery></Recovery>
+  } ,
+  {
+      path : '/reset' , 
+      element : <Reset></Reset>
+  } ,
+  {
+      path : '/pagenotfound' , 
+      element : <PageNotFound></PageNotFound>
+  } ,
+
+])
+
+export default function App() {
+  return (
+    <main>
+      <RouterProvider router={router}></RouterProvider>
+    </main>
+  )
+}
---
+++ client/login_app/src/main.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css';
+
+// const router = createBrowserRouter([
+
+//     {
+//         path : '/' , 
+//         element : <div>Root Routes</div>
+//     } ,
+//     {
+//         path : '/Resgister' , 
+//         element : <div>Resgister  Routes</div>
+//     } ,
+
+// ])
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+    <App />
+ 
+)
