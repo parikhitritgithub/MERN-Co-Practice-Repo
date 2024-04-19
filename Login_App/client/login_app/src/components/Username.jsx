05d9f267898721abac23833329f7a4557c4c891b
parikhitritgithub <parikhitkurmi44@gmail.com>
2024-04-19 22:22:12 +0530

    add all

--- Login_App/client/login_app/src/components/Username.jsx
+++ Login_App/client/login_app/src/components/Username.jsx
@@ -2,8 +2,23 @@ import React from 'react'
 
 export default function Username() {
   return (
-    <div>
-      username
-    </div>
+      <div className='container mx-auto'>
+        <div className='flex justify-center items-center bg-red-500 h-screen  '>
+          <div>
+            <div className='title flex flex-col item-center'>
+              <h4 className='text-5xl font-bold'>Hello Again </h4>
+              <span className='py-4 text-xl w-2/3 text-centre text-gray-5'>
+                Explore  More by connecting with us 
+              </span>
+            </div>
+
+            <form className='py-1'>
+              <div className='bg-blue-400'></div>
+            </form>
+
+
+          </div>
+        </div>
+      </div>
   )
 }
