1860ede8c69b9e4f27b0b4ea71e7d4687e689eed
parikhitritgithub <parikhitkurmi44@gmail.com>
2024-04-20 12:17:26 +0530

    add all

--- Login_App/client/login_app/src/components/Username.jsx
+++ Login_App/client/login_app/src/components/Username.jsx
@@ -4,8 +4,13 @@ import { Link } from 'react-router-dom'
 export default function Username() {
   return (
     <div className=' flex  items-center justify-center  h-screen  '>
-      <div className=' border-2 border-gray-400 w-2/6  h-2/3   rounded-3xl transition duration-500  hover:scale-105 '>
-        <div></div>
+      <div className=' flex justify-center border-2 border-gray-400 w-2/6  h-2/3   rounded-3xl transition duration-500  hover:scale-105 '>
+        <div className='flex flex-col justify-center items-center w-2/4 h-2/6'>
+          <h4 className='text-3xl py-1'>Hello Again !</h4>
+          <h1> explore more </h1>
+          <h1> By connecting with us </h1>
+        </div>
+        
 
 
       </div>
