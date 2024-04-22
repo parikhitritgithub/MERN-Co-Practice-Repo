b2361c532a2ec280a112f67d8476b8ba5f93985e
parikhitritgithub <parikhitkurmi44@gmail.com>
2024-04-22 07:35:29 +0530

    add all

--- Login_App/client/login_app/src/components/Username.jsx
+++ Login_App/client/login_app/src/components/Username.jsx
@@ -8,7 +8,7 @@ export default function Username() {
 
   const formik = useFormik ({
     initialValues : {
-      username : '' 
+      username : ' ' 
     },
     validate : UsernameValidate ,
     validateOnBlur : false ,
@@ -20,7 +20,7 @@ export default function Username() {
 
   return (
     <div className=' flex  items-center justify-center  h-screen '>
-      <Toaster position='top-center' reverseOrder={false} ></Toaster>
+      <Toaster position='top-center' reverseOrder={false}></Toaster>
       <div className=' flex flex-col  justify-center items-center bg-gray-300 bg-opacity-50 border-2 border-gray-400 w-2/6  h-2/3 rounded-3xl '>
         <div className='flex flex-col justify-center items-center w-2/4 h-2/6'>
           <h4 className='text-3xl py-1'>Hello Again !</h4>
@@ -31,10 +31,10 @@ export default function Username() {
         <div className=' flex flex-col items-center justify-center w-2/4 h-3/6 py-3 '>
           <form className='flex flex-col items-center justify-center py-3 ' onSubmit={formik.handleSubmit} > 
             <input  {...formik.getFieldProps('username')} type="text" placeholder='Username'
-            className=' rounded-lg  focus:outline-none  font-Nunito Sans transition duration-500  hover:scale-105 '
+            className=' rounded-lg w-full h-8 focus:outline-none  font-Nunito Sans transition duration-500  hover:scale-105 text-center  '
             />
-            <button className=' py-1 rounded-md mt-9 border-solid border-2 border-sky-500 w-1/3 h-1/2'>Let's Go</button>
-            <span className='py-3 mt-7'>Not a member ! <Link to="/register" className='text-red-500'> Register Now </Link></span>
+            <button className=' py-1 rounded-md mt-5 border-solid border-2 border-sky-500 w-full '>Let's Go</button>
+            <span className='py-3 mt-7'>Not a member ! <Link to="/register" className='text-red-500'>Register Now </Link></span>
           </form>
         </div>
         
